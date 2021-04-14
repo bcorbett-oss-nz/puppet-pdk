f07ad92ed55607f2004284844ebfcb392402d014
Bob Corbett <bcorbett@azure.oss-group.co.nz>
2021-04-14 14:35:48 +1200

    first commit

---
+++ manifests/linux.pp
@@ -0,0 +1,8 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include bobc_test_temp::linux
+class bobc_test_temp::linux {
+}
